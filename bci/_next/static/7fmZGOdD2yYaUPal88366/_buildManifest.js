4aa912ba15a7c93a5b9a5a348d8a826b2f7996c4
ejabu <muhammadfahreza@gmail.com>
2020-09-17 14:34:44 +0700

    -

--- bci/_next/static/7fmZGOdD2yYaUPal88366/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002F05d954cf.427b52c39a44e070b5f8.js","static\u002Fchunks\u002Fpages\u002Findex-f30880f62b2f48c43d6e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0d8b37c31ac76a3f5ea0.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
